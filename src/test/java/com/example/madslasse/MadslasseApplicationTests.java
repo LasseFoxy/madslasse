81ebb00584483d8eb8bbedd3bfecc07edd72a6af
LasseFoxy <lafo0001@stud.kea.dk>
2024-03-22 11:23:33 +0100

    Initial commit

---
+++ src/test/java/com/example/madslasse/MadslasseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madslasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadslasseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
